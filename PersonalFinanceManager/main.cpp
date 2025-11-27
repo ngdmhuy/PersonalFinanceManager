e54455aeaa2930bbae3e2127c29fc46560f26b71
Nguyen Dinh Minh Huy <ndmhuy2521@apcs.fitus.edu.vn>
2025-11-27 10:51:34 +0700

    First init

---
+++ PersonalFinanceManager/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PersonalFinanceManager
+//
+//  Created by Huy Nguyen on 27/11/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
