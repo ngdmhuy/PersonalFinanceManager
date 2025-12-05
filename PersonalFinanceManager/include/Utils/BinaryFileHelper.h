fbf6e744900031c16c7d237d3429b4b255447270
Nguyen Dinh Minh Huy <ndmhuy2521@apcs.fitus.edu.vn>
2025-12-05 11:55:15 +0700

    Add empty BinaryFileHelper.h

---
+++ PersonalFinanceManager/include/Utils/BinaryFileHelper.h
@@ -0,0 +1,12 @@
+//
+//  BinaryFileHelper.h
+//  PersonalFinanceManager
+//
+//  Created by Nguyen Dinh Minh Huy on 30/11/25.
+//
+
+#ifdef BinaryFileHelper_h
+#define BinaryFileHelper_h
+
+#endif // !BinaryFileHelper_h
+
