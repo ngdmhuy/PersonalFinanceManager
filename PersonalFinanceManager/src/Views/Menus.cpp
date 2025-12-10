3ea6c6f97e3ebaed42deef4a4702cd18193f8624
Toan-Tran-lab <tranductoan2k7nt@gmail.com>
2025-12-10 07:13:05 +0700

    Add headers for each individual menu in Menu.cpp

---
+++ PersonalFinanceManager/src/Views/Menus.cpp
@@ -0,0 +1,11 @@
+#include "Views/Menus.h"
+
+const string Menus::MAIN_MENU_TITLE = "--- MAIN MENU ---";
+const string Menus::MAIN_MENU_1 = "1. Add Income";
+const string Menus::MAIN_MENU_2 = "2. Add Expense";
+const string Menus::MAIN_MENU_3 = "3. View Statistics";
+const string Menus::MAIN_MENU_4 = "4. Manage Categories";
+const string Menus::MAIN_MENU_5 = "5. Exit";
+
+const string Menus::ADD_INCOME_TITLE = "=== ADD INCOME ===";
+const string Menus::SELECT_SOURCE_PROMPT = "Enter Income Source ID: ";
