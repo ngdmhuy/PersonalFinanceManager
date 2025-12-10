d7ea39c5f180273151ebca3415253ddaa138118d
Toan-Tran-lab <tranductoan2k7nt@gmail.com>
2025-12-10 07:07:42 +0700

    Add class Menus in Menus.h and empty file Menus.cpp

---
+++ PersonalFinanceManager/include/Views/Menus.h
@@ -0,0 +1,26 @@
+#ifndef MENUS_H
+#define MENUS_H
+#include <string>
+
+using namespace std;
+
+class Menus {
+public:
+    // Main Dashboard Menu
+    static const string MAIN_MENU_TITLE;
+    static const string MAIN_MENU_1;
+    static const string MAIN_MENU_2;
+    static const string MAIN_MENU_3;
+    static const string MAIN_MENU_4;
+    static const string MAIN_MENU_5;
+    
+    // Add Income Form
+    static const string ADD_INCOME_TITLE;
+    static const string SELECT_SOURCE_PROMPT;
+    
+    // Add Expense Form  
+    static const string ADD_EXPENSE_TITLE;
+    static const string SELECT_CATEGORY_PROMPT;
+};
+
+#endif
